1b0aaad1c94cb73cb8c73bb18b65bbaca5a6f85c
daniil317 <daniilbobrov2013@gmail.com>
2026-06-20 01:04:43 +0300

    Update ExampleModClient.java

--- src/client/java/com/example/client/ExampleModClient.java
+++ src/client/java/com/example/client/ExampleModClient.java
@@ -1,10 +1,32 @@
 package com.example.client;
 
+import com.example.CustomGuiScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class ExampleModClient implements ClientModInitializer {
+
+    private static KeyBinding openMenuKey;
+
     @Override
     public void onInitializeClient() {
-        // Оставляем пустым, всё перенесли
+        // Теперь регистрируем строго на ПРАВЫЙ ШИФТ (RIGHT_SHIFT)
+        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.examplemod.open_menu",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_RIGHT_SHIFT, 
+                "category.examplemod.cheats"
+        ));
+
+        // Отслеживаем нажатие кнопки в игре
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (openMenuKey.wasPressed() && client.player != null) {
+                client.setScreen(new CustomGuiScreen());
+            }
+        });
     }
 }

39f278582c7e2e1f25f5a4d34b35ee06f7f4cb84
daniil317 <daniilbobrov2013@gmail.com>
2026-06-20 01:55:50 +0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,22 +1,10 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.text.Text;
 
 public class ExampleMod implements ModInitializer {
     @Override
     public void onInitialize() {
-        // Регистрируем команду /cheat прямо через серверный обработчик
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            dispatcher.register(CommandManager.literal("cheat")
-                .executes(context -> {
-                    // Этот текст увидят все, когда ты введешь команду
-                    context.getSource().sendMessage(Text.literal("§c[Cheat] §aФункция успешно активирована!"));
-                    return 1;
-                })
-            );
-        });
+        System.out.println("Mod loaded successfully!");
     }
 }

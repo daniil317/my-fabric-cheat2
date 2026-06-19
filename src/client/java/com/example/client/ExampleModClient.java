245dbfc348046da6d2eadcf0aa0ec1e508d1446a
daniil317 <daniilbobrov2013@gmail.com>
2026-06-20 00:54:54 +0300

    Update ExampleModClient.java

--- src/client/java/com/example/client/ExampleModClient.java
+++ src/client/java/com/example/client/ExampleModClient.java
@@ -1,10 +1,32 @@
 package com.example.client;
 
+import com.example.CustomGuiScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class ExampleModClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+
+    private static KeyBinding openMenuKey;
+
+    @Override
+    public void onInitializeClient() {
+        // Регистрируем клавишу 'O' для открытия меню
+        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.examplemod.open_menu",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_O, 
+                "category.examplemod.cheats"
+        ));
+
+        // Проверяем нажатие в каждом тике
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (openMenuKey.wasPressed() && client.player != null) {
+                client.setScreen(new CustomGuiScreen());
+            }
+        });
+    }
 }

36add6cf6119cd88af4d3d738e5e29d3a7973e27
daniil317 <daniilbobrov2013@gmail.com>
2026-06-20 01:19:25 +0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,15 +1,54 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 
 public class ExampleMod implements ModInitializer {
-    public static final String MOD_ID = "modid";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    private static KeyBinding openMenuKey;
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Hello Fabric world! Core initialized.");
+        // Регистрируем кнопку на Правый Шифт прямо тут
+        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.examplemod.open_menu",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_RIGHT_SHIFT, 
+                "category.examplemod.cheats"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (openMenuKey.wasPressed() && client.player != null) {
+                client.setScreen(new InsideGuiScreen());
+            }
+        });
+    }
+
+    // Кастомное меню
+    public static class InsideGuiScreen extends Screen {
+        public InsideGuiScreen() {
+            super(Text.literal("My Custom Menu"));
+        }
+
+        @Override
+        protected void init() {
+            this.addDrawableChild(ButtonWidget.builder(Text.literal("Test Button"), button -> {
+                if (this.client != null && this.client.player != null) {
+                    this.client.player.sendMessage(Text.literal("Кнопка работает!"), false);
+                }
+            }).dimensions(this.width / 2 - 100, this.height / 2 - 10, 200, 20).build());
+        }
+
+        @Override
+        public boolean shouldCloseOnEsc() {
+            return true;
+        }
     }
 }

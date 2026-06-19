5b81c16fc9fb4f358067a76f79937561a178166d
daniil317 <daniilbobrov2013@gmail.com>
2026-06-20 00:50:47 +0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,19 +1,30 @@
 package com.example;
 
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ClientModInitializer {
-
+public class ExampleMod implements ModInitializer, ClientModInitializer {
+    
+    public static final String MOD_ID = "modid";
+    public static final Logger LOGGER = LoggerFactory.LoggerFactory.getLogger(MOD_ID);
     private static KeyBinding openMenuKey;
 
+    // Этот метод нужен для главного запуска мода
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Fabric world! Mod initialized.");
+    }
+
+    // Этот метод отвечает за кнопку открытия меню на клиенте
     @Override
     public void onInitializeClient() {
-        // Регистрируем клавишу 'O' для открытия меню
         openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.examplemod.open_menu",
                 InputUtil.Type.KEYSYM,
@@ -21,7 +32,6 @@ public class ExampleMod implements ClientModInitializer {
                 "category.examplemod.cheats"
         ));
 
-        // Каждый игровой тик проверяем нажатие кнопки
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (openMenuKey.wasPressed() && client.player != null) {
                 client.setScreen(new CustomGuiScreen());

88f68a5cbdcc68effa15349095b4ccdcd96d654d
daniil317 <daniilbobrov2013@gmail.com>
2026-06-20 01:00:59 +0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
@@ -10,21 +9,16 @@ import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer, ClientModInitializer {
+public class ExampleMod implements ModInitializer {
     
     public static final String MOD_ID = "modid";
-    public static final Logger LOGGER = LoggerFactory.LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     private static KeyBinding openMenuKey;
 
-    // Этот метод нужен для главного запуска мода
     @Override
     public void onInitialize() {
-        LOGGER.info("Hello Fabric world! Mod initialized.");
-    }
+        LOGGER.info("Mod initialized.");
 
-    // Этот метод отвечает за кнопку открытия меню на клиенте
-    @Override
-    public void onInitializeClient() {
         openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.examplemod.open_menu",
                 InputUtil.Type.KEYSYM,

0227c673c4f416ca340c98e161217f1ad15b1c8e
daniil317 <daniilbobrov2013@gmail.com>
2026-06-20 01:03:18 +0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,35 +1,15 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-    
     public static final String MOD_ID = "modid";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    private static KeyBinding openMenuKey;
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Mod initialized.");
-
-        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.examplemod.open_menu",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_O, 
-                "category.examplemod.cheats"
-        ));
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (openMenuKey.wasPressed() && client.player != null) {
-                client.setScreen(new CustomGuiScreen());
-            }
-        });
+        LOGGER.info("Hello Fabric world! Core initialized.");
     }
 }
